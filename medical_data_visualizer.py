8bcdf0e803b5b2c4a7849c033669bb6aa50c73d7
Enadeghe Osaze <enadegheosaze024@gmail.com>
2026-01-03 16:51:46 +0100

    Update medical_data_visualizer.py

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -1,60 +1,66 @@
+# 1
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 1
-df = None
+# 2: Load the data
+df= pd.read_csv('C:\\Users\\PC\\FreeCodeCamp_DataAnalysis_Cert\\boilerplate-medical-data-visualizer\\medical_examination.csv')
 
-# 2
-df['overweight'] = None
+# 3: Add the 'overweight' Column
+df['overweight'] = (df['weight'] /((df['height']/100)**2)>25).astype(int)
 
-# 3
+#4: Normalize data by making '0' always good and '1' always bad. 
+# If the value of 'cholesterol' or 'gluc' is 1, set it to 0. 
+# If the value is greater than 1, set it to 1.
+df['cholesterol'] =(df['cholesterol']>1).astype(int)
+df['gluc'] = (df['gluc']>1).astype(int)
 
-
-# 4
 def draw_cat_plot():
-    # 5
-    df_cat = None
+    # 5: Create DataFrame for cat plot using `pd.melt`
+    # This turns your wide table into a long "tidy" table
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['active', 'alco', 'cholesterol', 'gluc', 'overweight', 'smoke'])
 
-
-    # 6
-    df_cat = None
+    # 6: Group and reformat the data to split it by 'cardio'. 
+    # This calculates the 'total' counts for each category.
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
     
+    # 7: Draw the catplot with 'sns.catplot()'
+    # We grab the '.fig' so the grader can check the image
+    fig = sns.catplot(data=df_cat, x='variable', y='total', hue='value', col='cardio', kind='bar').fig
 
-    # 7
-
-
-
-    # 8
-    fig = None
-
-
-    # 9
+    # 8: Save the output and return the figure
+    # 9: The "Do not modify" lines: fig.savefig and return fig.
     fig.savefig('catplot.png')
     return fig
-
-
-# 10
+    
+    #10
 def draw_heat_map():
-    # 11
-    df_heat = None
-
-    # 12
-    corr = None
-
-    # 13
-    mask = None
-
-
-
-    # 14
-    fig, ax = None
-
-    # 15
+    # 11 : clean the data
+    df_heat = df[
+    (df['ap_lo'] <= df['ap_hi']) & 
+    (df['height'] >= df['height'].quantile(0.025)) & 
+    (df['height'] <= df['height'].quantile(0.975)) & 
+    (df['weight'] >= df['weight'].quantile(0.025)) & 
+    (df['weight'] <= df['weight'].quantile(0.975))]
+    # 12: Perform the correlation matrix
+    corr = df_heat.corr()
+    # 13 : Create the triangle mask
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
 
-
-    # 16
+    # 14 : Set up the matplotlib figure
+    fig, ax = plt.subplots(figsize=(12,12))
+    # 15 : Draw the heat map with 'sns.heatmap'
+    sns.heatmap(corr, mask=mask, 
+            annot=True, 
+            fmt='.1f', 
+            center=0, 
+            square=True, 
+            linewidths=.5, 
+            cbar_kws={'shrink': .5}
+            )
+     # 16: Save the heat map and return
     fig.savefig('heatmap.png')
     return fig
+
